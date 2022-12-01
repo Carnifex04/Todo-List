dc0b081ec937e5c6ad657de06a048035037d711b
hriday <guptahriday4444@gmail.com>
2022-12-01 20:27:30 +0530

    added TaskDetail class and the corresponding HTML file

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 from .models import Task
 
@@ -10,3 +12,7 @@ from .models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
